0a2c458b023d81032f4f3773c960dfe7679252f0
Pablo Henrique <pablohen7ique@gmail.com>
2023-04-06 23:02:15 -0300

    Inicio do Exercicio

---
+++ src/main/java/com/primeirotrampodev/Anuncio/Carro/AnuncioCarroApplication.java
@@ -0,0 +1,13 @@
+package com.primeirotrampodev.Anuncio.Carro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnuncioCarroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnuncioCarroApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/primeirotrampodev/Anuncio/Carro/AnuncioCarroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.primeirotrampodev.Anuncio.Carro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnuncioCarroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
